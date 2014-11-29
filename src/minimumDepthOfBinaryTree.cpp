fb5fdf302f5a43681d30bc83f3fd36e21cc57195
liuzongming111@gmail.com <liuzongming111@gmail.com>
2014-11-29 23:49:07 +0800

    first commit

---
+++ src/minimumDepthOfBinaryTree.cpp
@@ -0,0 +1,11 @@
+// Given a binary tree, find its minimum depth.
+
+// The minimum depth is the number of nodes along 
+// the shortest path from the root node down to the nearest leaf node.
+
+class Solution{
+public:
+	int minDepth(TreeNode *root){
+		
+	}
+}
